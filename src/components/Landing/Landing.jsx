db685288b75b8d3c80fd31d23c919f234aff14ed
gkourouma <gkourouma2@gmail.com>
2025-04-05 00:38:41 -0500

    Updated landing page with toggleForm conditionals do decide which form to show based on if the user wants to sign in or sign up. Removed border and background from signup form.

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import SignInForm from "../SignInForm/SignInForm";
 import SignUpForm from "../SignUpForm/SignUpForm"
 import styles from "./Landing.module.css";
 
 const Landing = () => {
+  const [isSignUp, setIsSignUp] = useState(true)
+
+  const toggleForm = () => {setIsSignUp(!isSignUp)}
   return (
     <main className={styles.landingContainer}>
       <div className={styles.videoContainer}>
@@ -10,9 +14,11 @@ const Landing = () => {
       </div>
 
       <div className={styles.signUpForm}>
-      <h1>Welcome to Rotten<span>Oranges</span></h1>
-      <p>Join the Rotten Oranges community to discover the latest movie reviews, ratings, and recommendations. Sign up now to start exploring</p>
-        <SignUpForm />
+      <h1 className={styles.heading}>Welcome to Rotten<span className={styles.oranges}>Oranges</span></h1>
+      <p className={styles.welcomeMessage}>Join the Rotten Oranges community to discover the latest movie reviews, ratings, and recommendations. Sign up now to start exploring</p>
+        {isSignUp ? <SignUpForm /> : <SignInForm />}
+        <p>{isSignUp ? 'Already have an account? ' : "Don't have an account yet? "}</p>
+        <button onClick={toggleForm}> {isSignUp ? 'Sign In': 'Sign Up'}</button>
       </div>
     </main>
   );
