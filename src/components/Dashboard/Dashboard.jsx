0df479027fc16f8c52ce56f046391c039b6fc9d5
gkourouma <gkourouma2@gmail.com>
2025-04-08 16:00:00 -0500

    Created dashboard page with TMDB API, added styling to the dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,34 +1,88 @@
-import { useEffect, useContext, useState } from 'react';
-import { UserContext } from '../../contexts/UserContext';
-import {index} from '../../services/userService';
+import { useEffect, useContext, useState } from "react";
+import { UserContext } from "../../contexts/UserContext";
+import styles from "./Dashboard.module.css";
+import { Link } from "react-router";
+
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
-  const [users, setUsers] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [tvShows, setTvShows] = useState([]);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const fetchedUsers = await index();
-        setUsers(fetchedUsers);
-      } catch (error) {
-        console.log(error)
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_BEARER_TOKEN}`
       }
-    }
-    if (user) fetchUsers();
-  }, [user]);
+    };
+
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
+      options
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.results); 
+        setMovies(data.results); 
+      })
+      .catch(error => console.error(error));
+
+    fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc', options)
+        .then(res => res.json())
+        .then(data => {
+          const filteredTvShow = data.results.filter(tvShow => tvShow.name !== "Please Put Them On, Takamine-san");
+          console.log(filteredTvShow);
+          setTvShows(filteredTvShow);
+        })
+      .catch(error => console.error(error));
+  }, []);
 
   return (
-    <main>
-      <h1>Welcome, {user.username}</h1>
-      <p>
-        This is the dashboard page where you can see a list of all the users.
-      </p>
-      {
-        users.map(user => (
-          <li key={user._id}>{user.username}</li>
-        ))
-      }
+    <main className={styles.dashboardContainer}>
+      {/* <h1 className={styles.heading}>Welcome {user.username}</h1> */}
+      <p className={styles.discover}>Trending Movies </p>
+      <div className={styles.moviesList}>
+        {movies.length === 0 ? (
+          <p>No movies to display</p>
+        ) : (
+          movies.map((movie) => (
+            <Link to={`/movie/${movie.id}`} key={movie.id} className={styles.movieCard}>
+            <div key={movie.id} className={styles.movieCard}>
+              {/* <h2 className={styles.movieTitle}>{movie.title}</h2> */}
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
+                alt={movie.title}
+                className={styles.moviePoster}
+              />
+              {/* <p>{movie.overview}</p> */}
+            </div>
+            </Link>
+          ))
+        )}
+      </div>
+
+      <p className={styles.discover}>Trending TV Shows</p>
+      <div className={styles.tvShowList}>
+        {tvShows.length === 0 ? (
+          <p>No movies to display</p>
+        ) : (
+          tvShows.map((tvShow) => (
+            <Link to={`/tv/${tvShow.id}`} key={tvShow.id} className={styles.tvShowCard}>
+            <div key={tvShow.id} className={styles.tvShowCard}>
+              {/* <h2 className={styles.tvShowTitle}>{tvShow.name}</h2> */}
+              <img
+                src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`} 
+                alt={tvShow.title}
+                className={styles.tvShowPoster}
+              />
+              {/* <p>{movie.overview}</p> */}
+            </div>
+            </Link>
+          ))
+        )}
+      </div>
     </main>
   );
 };
