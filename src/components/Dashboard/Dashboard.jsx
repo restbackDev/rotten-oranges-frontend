f9d327441b143dd5985c7d2c8d33e531ac62a9e7
restbackDev <jason.rbdev@gmail.com>
2025-04-11 13:54:41 -0700

    remove code comments

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -28,27 +28,6 @@ const Dashboard = () => {
         Authorization: `Bearer ${import.meta.env.TMDB_BEARER_TOKEN}`
       }
     };
-
-    // fetch(
-    //   'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc',
-    //   options
-    // )
-    //   .then(res => res.json())
-    //   .then(data => {
-    //     console.log(data.results); 
-    //     setMovies(data.results); 
-    //   })
-    //   .catch(error => console.error(error));
-
-// fetching TV shows from TMDb directly MICHELLE
-//     fetch('https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc', options)
-//         .then(res => res.json())
-//         .then(data => {
-//           const filteredTvShow = data.results.filter(tvShow => tvShow.name !== "Please Put Them On, Takamine-san");
-//           console.log(filteredTvShow);
-//           setTvShows(filteredTvShow);
-//         })
-//       .catch(error => console.error(error));
   }, []);
 
   return (
@@ -71,44 +50,6 @@ const Dashboard = () => {
           ))
         )}
       </div>
-
-      {/* <p className={styles.discover}>Trending Movies </p>
-      <div className={styles.moviesList}>
-        {movies && Array.isArray(movies) && movies.length > 0 ? (
-          <p>No movies to display</p>
-        ) : (
-          movies.map((movie) => (
-            <Link to={`/movie/${movie.id}`} key={movie.id} className={styles.movieCard}>
-            <div key={movie.id} className={styles.movieCard}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-                alt={movie.title}
-                className={styles.moviePoster}
-              />
-            </div>
-            </Link>
-          ))
-        )}
-      </div> 
-
-      <p className={styles.discover}>Trending TV Shows</p>
-      <div className={styles.tvShowList}>
-        {tvShows.length === 0 ? (
-          <p>No movies to display</p>
-        ) : (
-          tvShows.map((tvShow) => (
-            <Link to={`/tv/${tvShow.id}`} key={tvShow.id} className={styles.tvShowCard}>
-            <div key={tvShow.id} className={styles.tvShowCard}>
-              <img
-                src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`} 
-                alt={tvShow.title}
-                className={styles.tvShowPoster}
-              />
-            </div>
-            </Link>
-          ))
-        )}
-      </div>  */}
     </main>
   );
 };
